11f8a202e80ccbe7b37fb79b26dfda567a60f6b3
Lindsay Kinn <lindsay.kinn@gmail.com>
2020-11-19 14:11:33 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,13 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS =[]
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+   unique_brands = BRANDS.uniq!
+   unique_brands
   end
 
   def cobble
@@ -11,4 +16,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  
 end
